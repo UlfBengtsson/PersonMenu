0180569693aeb9e85feb594df89b706612a92c6b
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-01-29 08:50:04 +0100

    Add project files.

---
+++ PersonMenu/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonMenu
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string lastName;
+        int age;
+
+        public Person(){}   // Zero Constructor - If we don´t create any constructor then C# compiler will add this one for ous.
+
+        public Person(string firstName, string lastName)    // When we Overload a Constructor or Method, it the input types that are the key for the computer to see the diffrence between them(not the verible name).
+        {
+            FirstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello! My name is " + FirstName + " " + lastName);
+        }
+    }
+}
---
+++ PersonMenu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Person ulf = new Person();
+            ulf.FirstName = "Ulf";
+            ulf.lastName = "Bengtsson";
+            ulf.SayHello();
+
+            Person erik = new Person("Erik", "Svensson");   // Smart way to use a Constructor(Overload) to simplefie our work.11
+            erik.SayHello();
+            */
+
+
+            List<Person> people = new List<Person>();
+            people.Add(new Person("Ulf", "Bengtsson")); // Adds ulf to the list of people, we dont need to create a verible first and then store it in the list.
+            people.Add(new Person("Erik", "Svensson"));
+            people.Add(new Person("Niklas","Yngvesson"));
+            people.ForEach(p => p.SayHello());
+
+            Console.ReadKey();
+        }
+    }
+}
